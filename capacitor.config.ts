2450bba6d234bbf681df27e58205a12ca22d1877
Pari-Dhanani <136296284+Pari-Dhanani@users.noreply.github.com>
2024-05-12 14:13:43 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bloodyMary',
+  webDir: 'www'
+};
+
+export default config;
